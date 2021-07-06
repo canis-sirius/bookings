15a38978f6d4786b2fd36b08ed0c6a8a09192cfa
canis-sirius <davidtruong.dev@gmail.com>
2021-07-05 22:10:28 -0400

    feat: add migration

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,28 +1,62 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
-
-	"github.com/canis-sirius/bookings/pkg/config"
-	"github.com/canis-sirius/bookings/pkg/handlers"
-	"github.com/canis-sirius/bookings/pkg/render"
+	"github.com/canis-sirius/bookings/internal/config"
+	"github.com/canis-sirius/bookings/internal/handlers"
+	"github.com/canis-sirius/bookings/internal/helpers"
+	"github.com/canis-sirius/bookings/internal/models"
+	"github.com/canis-sirius/bookings/internal/render"
 )
 
 const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
-// main is the main application function
+// main is the main function
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	// what am I going to put in the session
+	gob.Register(models.Reservation{})
+
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	// set up the session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -34,6 +68,7 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -42,14 +77,7 @@ func main() {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	return nil
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,10 @@
 module github.com/canis-sirius/bookings
 
-go 1.16
+go 1.15
 
 require (
-	github.com/alexedwards/scs/v2 v2.4.0 // indirect
-	github.com/go-chi/chi v1.5.4 // indirect
-	github.com/justinas/nosurf v1.1.1 // indirect
+	github.com/alexedwards/scs/v2 v2.4.0
+	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
+	github.com/go-chi/chi v1.5.1
+	github.com/justinas/nosurf v1.1.1
 )
